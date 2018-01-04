6358c128b8e19f9141120d7c453689a110d6945f
yrostem <yrostem@roc-dev.com>
2018-01-05 00:23:46 +0100

    opdr 2.1 af

---
+++ h2/2.1/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("aap1").src = "img/aap1.jpg";
+document.getElementById("aap2").src = "img/aap2.jpg";
