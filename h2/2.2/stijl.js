5aab073fe9ebd40d8908b0554d7b3e61f9616d09
yrostem <yrostem@roc-dev.com>
2018-01-05 00:35:35 +0100

    opdr 2.2 af

---
+++ h2/2.2/stijl.js
@@ -0,0 +1,4 @@
+document.getElementById("bl1").style.background = "cyan";
+document.getElementById("bl2").style.background = "green";
+document.getElementById("bl3").style.background = "yellow";
+document.getElementById("bl4").style.background = "red";
